6bb188dfd11af295c86d3157b1f525da4b72282a
Gega <latariagega1@gmail.com>
2022-07-25 19:17:22 +0400

    fixing login part 1

--- src/components/nav-items/login/Login.js
+++ src/components/nav-items/login/Login.js
@@ -1,79 +1,125 @@
 import React, { useState } from "react";
 import "./Login.css";
 
-function Login() {
-  const [errorMessages, setErrorMessages] = useState({});
-  const [isSubmitted, setIsSubmitted] = useState(false);
+const database = [
+  {
+    name: "Gega",
+    password: "1234",
+  },
+  {
+    name: "Anano",
+    password: "123",
+  },
+];
 
-  // User Login info
-  const database = [
-    {
-      username: "user1",
-      password: "pass1"
-    },
-    {
-      username: "user2",
-      password: "pass2"
-    }
-  ];
+const handleSubmit = (event) => {
+  event.preventDefault();
+};
 
-  const errors = {
-    uname: "invalid username",
-    pass: "invalid password"
-  };
+const handleLogin = (username, pass) => {
+  if (username === database[0].name && pass === database[0].password) {
+    console.log("Success");
+  }
+};
 
-  const handleSubmit = (event) => {
-    //Prevent page reload
-    event.preventDefault();
-
-    let { uname, pass } = document.forms[0];
-
-    const userData = database.find((user) => user.username === uname.value);
-
-    if (userData) {
-      if (userData.password !== pass.value) {
-        setErrorMessages({ name: "pass", message: errors.pass });
-      } else {
-        setIsSubmitted(true);
-      }
-    } else {
-      setErrorMessages({ name: "uname", message: errors.uname });
-    }
-  };
-
-  const renderErrorMessage = (name) =>
-    name === errorMessages.name && (
-      <div className="error">{errorMessages.message}</div>
-    );
-
-  const renderForm = (
-    <div className="form">
-      <form onSubmit={handleSubmit}>
-        <div className="input-container">
-          <label>სახელი </label>
-          <input type="text" name="uname" required />
-          {renderErrorMessage("uname")}
+const Login = () => {
+  return (
+    <div className="login-container">
+      <form className="login-form">
+        <div className="login-form__item">
+          <p className="login-form__item__title">სახელი</p>
+          <input className="login-form__item__input" type={"email"}></input>
         </div>
-        <div className="input-container">
-          <label>პაროლი </label>
-          <input type="password" name="pass" required />
-          {renderErrorMessage("pass")}
+        <div className="login-form__item">
+          <p className="login-form__item__title">პაროლი</p>
+          <input className="login-form__item__input" type={"password"}></input>
         </div>
-        <div className="button-container">
-          <input type="submit" />
+        <div>
+          <input
+            className="login-form__button"
+            type={"submit"}
+            onClick={handleSubmit}
+            value="Login"
+          ></input>
         </div>
       </form>
     </div>
   );
+};
 
-  return (
-    <div className="app">
-      <div className="login-form">
-        <div className="title">შესვლა</div>
-        {isSubmitted ? <div>User is successfully logged in</div> : renderForm}
-      </div>
-    </div>
-  );
-}
+// function Login() {
+//   const [errorMessages, setErrorMessages] = useState({});
+//   const [isSubmitted, setIsSubmitted] = useState(false);
+
+//   // User Login info
+//   const database = [
+//     {
+//       username: "user1",
+//       password: "pass1"
+//     },
+//     {
+//       username: "user2",
+//       password: "pass2"
+//     }
+//   ];
+
+//   const errors = {
+//     uname: "invalid username",
+//     pass: "invalid password"
+//   };
+
+//   const handleSubmit = (event) => {
+//     //Prevent page reload
+//     event.preventDefault();
+
+//     let { uname, pass } = document.forms[0];
+
+//     const userData = database.find((user) => user.username === uname.value);
+
+//     if (userData) {
+//       if (userData.password !== pass.value) {
+//         setErrorMessages({ name: "pass", message: errors.pass });
+//       } else {
+//         setIsSubmitted(true);
+//       }
+//     } else {
+//       setErrorMessages({ name: "uname", message: errors.uname });
+//     }
+//   };
+
+//   const renderErrorMessage = (name) =>
+//     name === errorMessages.name && (
+//       <div className="error">{errorMessages.message}</div>
+//     );
+
+//   const renderForm = (
+//     <div className="form">
+//       <form onSubmit={handleSubmit}>
+//         <div className="input-container">
+//           <label>სახელი </label>
+//           <input type="text" name="uname" required />
+//           {renderErrorMessage("uname")}
+//         </div>
+//         <div className="input-container">
+//           <label>პაროლი </label>
+//           <input type="password" name="pass" required />
+//           {renderErrorMessage("pass")}
+//         </div>
+//         <div className="button-container">
+//           <input type="submit" />
+//         </div>
+//       </form>
+//     </div>
+//   );
+
+//   return (
+//     <div className="app">
+//       <div className="login-form">
+//         <div className="title">შესვლა</div>
+//         {isSubmitted ? <div>User is successfully logged in</div> : renderForm}
+//       </div>
+//     </div>
+//   );
+// }
 
 export default Login;
