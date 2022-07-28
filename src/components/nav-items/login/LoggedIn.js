e1689c1ddffffd6f8eff100759e9a09fd73e7c67
Gega <latariagega1@gmail.com>
2022-07-28 17:58:16 +0400

    implementing user profile

--- src/components/nav-items/login/LoggedIn.js
+++ src/components/nav-items/login/LoggedIn.js
@@ -1,13 +1,44 @@
 import React, { useEffect, useState } from "react";
 import Login from "./Login";
-// import { MyLink } from "../../footer/MyLink";
+import "./LoggedIn.css";
 
 const LoggedIn = ({ user, setUser }) => {
   const handleClick = () => {
     window.localStorage.removeItem("user");
     setUser("");
   };
-  return <button onClick={handleClick}>Log out</button>;
+  return (
+    <div>
+      <button className="sign-out-button" onClick={handleClick}>
+        Log out
+      </button>
+      <div className="address-box">
+        <div className="address-box__text">
+          <p>
+            <span className="address-box__text__username">
+              {user.username},{" "}
+            </span>{" "}
+            აქ ნახავ თურქეთის საწყობის მისამართსა და სხვა მნიშვნელოვან
+            ინფორმაციას, რაც დაგჭირდება თურქული საიტებიდან ნივთების გამოწერის
+            დროს. მარტივად დააკოპირე მარჯვენა კუთხეში მითითებული ღილაკის
+            მეშვეობით და თურქულ საიტზე მისამართის მითითებისას ჩასვი შესაბამის
+            ველში. თუ რაიმე კითხვა გაქვს, დაგვიკავშირდი. სასიამოვნო შოპინგს
+            გისურვებთ!
+          </p>
+        </div>
+      </div>
+      <div className="countries-box">
+        <div className="countries-box__list">
+          <div className="countries-box__list__item">
+            <h2>Turkey</h2>
+          </div>
+          <div className="countries-box__list__item">
+            <h2>Dubai</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LoggedIn;
