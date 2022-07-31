9c4807eff8e27d9ad4188e4d5c81b1ce32a7a8bc
Gega <latariagega1@gmail.com>
2022-07-31 10:19:44 +0400

    removing unused code

--- src/index.js
+++ src/index.js
@@ -1,21 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
-import { configureStore, applyMiddleware } from '@reduxjs/toolkit';
-import thunk from "redux-thunk";
-
 
 import App from "./components/App";
-import reducers from "./reducers";
 
-
-const store = configureStore({ reducer: reducers}, applyMiddleware(thunk));
-
-
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(
-    <Provider store={store}>
-        <App/>
-    </Provider>
-);
+root.render(<App />);
