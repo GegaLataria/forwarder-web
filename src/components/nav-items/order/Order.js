22110b2868e84ff7d2f9029deb0cdca16ffa88fc
Gega <latariagega1@gmail.com>
2022-08-05 13:19:12 +0400

    converting to responsive part 2

--- src/components/nav-items/order/Order.js
+++ src/components/nav-items/order/Order.js
@@ -20,18 +20,47 @@ const buildOrderScreen = () => {
         <div className="order__contact">
           <div className="order__contact__item">
             <p className="order__contact__item__header">ტელეფონი</p>
-            <p>555123456</p>
+            <p className="order__contact__item__input">555123456</p>
           </div>
           <div className="order__contact__item">
             <p className="order__contact__item__header">ელ.ფოსტა</p>
-            <p>info@company.ge</p>
+            <p className="order__contact__item__input">info@company.ge</p>
           </div>
           <div className="order__contact__item">
             <p className="order__contact__item__header">საათები: 10:00-19:00</p>
-            <p>ორშაბათი-პარასკევი</p>
+            <p className="order__contact__item__input">ორშაბათი-პარასკევი</p>
           </div>
         </div>
       </section>
+      <div className="services">
+        <div className="services__list">
+          <div className="services__list__item">
+            <h3 className="services__list__item__header">თურქეთის მისამართი</h3>
+            <p className="services__list__item__input">
+              რეგისტრაციისას მოგანიჭებთ თურქულ მისამართს
+            </p>
+          </div>
+          <div className="services__list__item">
+            <h3 className="services__list__item__header">გამოწერა</h3>
+            <p className="services__list__item__input">
+              გამოიწერე სასურველი ნივთები
+            </p>
+          </div>
+          <div className="services__list__item">
+            <h3 className="services__list__item__header">ტრანპორტირება</h3>
+            <p className="services__list__item__input">
+              შენ მიიღებ შეტყობინებას როცა ამანათი მოვა შენს მისამართზე
+            </p>
+          </div>
+          <div className="services__list__item">
+            <h3 className="services__list__item__header">გატანა</h3>
+            <p className="services__list__item__input">
+              შეგიძლია გაიტანო ოფისიდან ან ისარგებლო კურიერის მომსახურებით
+            </p>
+          </div>
+        </div>
+      </div>
+      <h2 className="youtube__header">რეგისტრაციის ინსტრუქცია</h2>
       <section className="youtube">
         <iframe
           className="youtube__link"
